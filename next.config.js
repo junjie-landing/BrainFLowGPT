fa4f60db821e7b0d74a55c1f2498b9c688dce6eb
junjie-landing <81201171+junjie-landing@users.noreply.github.com>
2024-11-23 12:24:35 -0800

    the chat explore runnable

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // ... any other config options you have
+}
+
+module.exports = nextConfig
